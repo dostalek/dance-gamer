21e03d0ae7399fec6699e2b90b1a53b0934884c9
Ryan Dostalek <ryanedostalek@gmail.com>
2025-06-28 04:55:21 -0700

    Add shared types

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 from typing import TypedDict
 
 
+class Match(TypedDict):
+    name: str
+    confidence: float
+    center: tuple[float, float]
+
+
 class Monitor(TypedDict):
     top: int
     left: int

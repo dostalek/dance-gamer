ea05f8e8365fa16f0dca9ed2eecdfc198734e83b
Ryan Dostalek <ryanedostalek@gmail.com>
2025-06-27 23:51:43 -0700

    Add methods to wait for for matches

--- screencapture.py
+++ screencapture.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.typing import NDArray
 from concurrent.futures import ThreadPoolExecutor
 import time
-from typing import TypedDict
+from typing import TypedDict, Callable
 
 
 class Monitor(TypedDict):
@@ -34,10 +34,10 @@ class ScreenCapture:
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
         return img_gray
 
-    def match_template(self, image: NDArray, template: tuple[str, NDArray]) -> Match:
+    def match_template(self, img: NDArray, template: tuple[str, NDArray]) -> Match:
         """Match image against template using TM_CCOEFF_NORMED."""
         template_name, template_image = template
-        res = cv2.matchTemplate(image, template_image, cv2.TM_CCOEFF_NORMED)
+        res = cv2.matchTemplate(img, template_image, cv2.TM_CCOEFF_NORMED)
         _, max_val, _, max_loc = cv2.minMaxLoc(res)
         top_left = max_loc
 
@@ -49,12 +49,12 @@ class ScreenCapture:
         }
 
     def match_templates(
-        self, image: NDArray, templates: list[tuple[str, NDArray]], max_workers=2
-    ):
+        self, img: NDArray, templates: list[tuple[str, NDArray]], max_workers: int
+    ) -> list[Match]:
         """Concurrently match image against provided templates."""
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
             futures = [
-                executor.submit(self.match_template, image, template)
+                executor.submit(self.match_template, img, template)
                 for template in templates
             ]
 
@@ -63,3 +63,43 @@ class ScreenCapture:
                 results.append(future.result())
 
             return results
+
+    def wait_for_match(
+        self,
+        templates: list[tuple[str, NDArray]],
+        threshold: float = 0.8,
+        delay: float = 0.1,
+        timeout: float | None = None,
+        callback: Callable[[Match], None] | None = None,
+    ) -> Match | None:
+        """Yield the current thread until a single match above confidence threshold is found."""
+        start_time = time.time()
+
+        while True:
+            if timeout and time.time() - start_time > timeout:
+                return None
+
+            img = self.screenshot()
+
+            matches = self.match_templates(img, templates, len(templates))
+
+            valid_matches = [
+                match for match in matches if match["confidence"] >= threshold
+            ]
+
+            if valid_matches:
+                # I really only want one match here, and by principle only one match
+                # above the threshold *should* be found. The match_templates method will
+                # always return a number of matches equal to the number of templates.
+                # I've opted to keep match_templates abstract, and handle matching a
+                # a single template above the threshold within this method.
+                match = valid_matches[0]
+                if callback:
+                    # TODO: Plan to pass click method with match
+                    callback(match)
+                return match
+            time.sleep(delay)
+
+    def wait_for_matches():
+        """Counterpart to wait_for_match. Will return all matches above threshold if needed."""
+        pass
